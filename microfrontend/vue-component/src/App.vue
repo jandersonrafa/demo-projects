c0bf508f9b257f200cf974e0d36eef442aa23c07
Janderson Rafael da Silva Rosa <janderson.rosa@cwi.com.br>
2023-10-30 19:52:53 -0300

    feat(microfrontend)

---
+++ microfrontend/vue-component/src/App.vue
@@ -0,0 +1,11 @@
+<script>
+import Settings from "./components/Settings.vue";
+
+export default {
+  template: `<h1>Demonstrations of Settings component</h1>`
+}
+
+document.body.appendChild(
+  new Settings()
+)
+</script>
