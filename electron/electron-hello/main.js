ec1efe03b9074ee7cd3caf9e245acbb1fbb6d7b8
jandersonrafa <jandersonrafa@gmail.com>
2025-01-21 09:56:53 -0300

    feat(electron): teste sistema de notificacao

--- electron/electron-hello/main.js
+++ electron/electron-hello/main.js
@@ -1,6 +1,18 @@
-const { app, BrowserWindow } = require('electron');
+const { app, Menu, BrowserWindow, Tray, nativeImage } = require('electron');
+const path = require('path');
+
+let tray = null;
+let mainWindow = null;
+let isQuiting;
+
+// Configura para inicializar com windows
+app.setLoginItemSettings({
+    openAtLogin: true
+})
+
+// Renderiza janela electron
 function createWindow() {
-    const mainWindow = new BrowserWindow({
+    mainWindow = new BrowserWindow({
         width: 800,
         height: 600,
         webPreferences: {
@@ -8,15 +20,56 @@ function createWindow() {
         },
     });
     mainWindow.loadFile('index.html');
+
+
+    mainWindow.on('minimize', function (event) {
+        event.preventDefault();
+        mainWindow.minimize();
+    });
+
+    // Da um overwrite no botao fechar para ao inves de parar app deixar minimizado e manter icone na bandeija
+    mainWindow.on('close', function (event) {
+        if (!isQuiting) {
+            event.preventDefault();
+            mainWindow.hide();
+        }
+        return false;
+    });
 }
-app.whenReady().then(createWindow);
-app.on('window-all-closed', () => {
-    if (process.platform !== 'darwin') {
-        app.quit();
-    }
+
+// Configura icone na area de notificacao (tray do windows)
+function createTray() {
+    const trayIcon = path.join(__dirname, 'icon.png')
+
+    tray = new Tray(trayIcon);
+    const contextMenu = Menu.buildFromTemplate([
+        {
+            label: 'Abrir Janela',
+            click: () => {
+                if (mainWindow === null) {
+                    createWindow();
+                }
+                mainWindow.show();
+            },
+        },
+        {
+            label: 'Sair',
+            click: () => {
+                app.quit();
+                isQuiting = true;
+            },
+        },
+    ]);
+
+    tray.setContextMenu(contextMenu);
+    tray.setToolTip('Aplicativo Electron');
+}
+
+app.whenReady().then(() => {
+    createWindow()
+    createTray();
 });
-app.on('activate', () => {
-    if (BrowserWindow.getAllWindows().length === 0) {
-        createWindow();
-    }
-});
+
+app.on('before-quit', function () {
+    isQuiting = true;
+  });
